0652341d458d50e11ebbaa699ffef38fc9c541a2
fatma <fatmatnt4@gmail.com>
2026-02-27 16:38:22 +0200

    Login

--- my-app/src/components/Login.js
+++ my-app/src/components/Login.js
@@ -1,77 +1,70 @@
 import { auth, db } from "../firebase"; 
 import { signInWithEmailAndPassword } from "firebase/auth";
 import { useState } from "react";
-import { doc, getDoc } from 'firebase/firestore';
+import { collection, query, where, getDocs} from 'firebase/firestore';
 import { Link, useNavigate } from 'react-router-dom';
+import back from './back.jpg';
 
 function Login() {
   const navigate = useNavigate(); 
-  const [email, setEmail] = useState("");
   const [password, setPassword] = useState("");
+  const [studentCode, setStudentCode] = useState("");
   const [error, setError] = useState('');
 
   const handleLogin = async (e) => {
     e.preventDefault();
     setError('');
-
-    if (!email || !password) {
-      setError("Please fill in all fields.");
-      return;
-    }
-
-    try {
-      const userCredential = await signInWithEmailAndPassword(auth, email.trim(), password.trim());
-      const user = userCredential.user;
+   
+    try{
       
-      const docRef = doc(db, "users", user.uid);
-      const docSnap = await getDoc(docRef);
+      const q = query(collection(db, "users"), where("studentCode", "==", studentCode));
+      const querySnapshot = await getDocs(q);
 
-      if (docSnap.exists()) {
-        const userData = docSnap.data();
-
-        if (userData.role === "admin") {
-          navigate('/admin-dashboard'); 
-        } else {
-          navigate('/dashboard'); 
-        }
-      } else {
-        setError("User data not found in database.");
+      if (querySnapshot.empty) {
+        setError("Student code not found");
+        return;
       }
 
-    } catch (err) {
-      console.error(err.code); 
-      if (err.code === 'auth/user-not-found' || err.code === 'auth/wrong-password' || err.code === 'auth/invalid-credential') {
-        setError("Invalid email or password.");
-      } else if (err.code === 'auth/invalid-email') {
-        setError("Please enter a valid email format.");
+      const userData = querySnapshot.docs[0].data();
+       const email = userData.email;
+      const userSignIn = await signInWithEmailAndPassword(auth, email, password);
+
+      if (userData.role == "admin") {
+        navigate("/admin");
+      } else if (userData.role == "student") {
+        navigate("/student");
       } else {
-        setError("Login failed. Please try again.");
-      } 
+        setError("Unknown role");
+      }
+    }catch(error){
+        setError(error.message);
     }
   };
 
   return (
-    <div style={{ padding: "40px", textAlign: "center", maxWidth: "400px", margin: "auto" }}>
-      <h2>Sign In</h2>
-      <form onSubmit={handleLogin}>
+     
+    <div style={{ padding: "280px", textAlign: "center", maxWidth: "1100px", margin: "auto",backgroundImage: `url(${back})`,backgroundSize: "cover" }}>
+      
+      <h2 >logIn</h2>
+      <form onSubmit={handleLogin} style={{ display: "flex", flexDirection: "column", alignItems: "center" }}>
         <input 
-          type="email" 
-          placeholder="Email" 
-          style={{ width: '100%', padding: '10px', marginBottom: '10px' }}
-          onChange={(e) => setEmail(e.target.value)} 
+          type="text" 
+          placeholder="id" 
+          style={{ width: '35%', padding: '10px', marginBottom: '10px' }}
+          onChange={(e) => setStudentCode(e.target.value)} 
           required 
         />
         <input 
           type="password" 
           placeholder="Password" 
-          style={{ width: '100%', padding: '10px', marginBottom: '10px' }}
+          style={{ width: '35%', padding: '10px', marginBottom: '10px' }}
           onChange={(e) => setPassword(e.target.value)} 
           required 
         />
         
         {error && <p style={{ color: '#ff4d4d', fontSize: '13px' }}>{error}</p>}
         
-        <button type="submit" style={{ padding: '10px 20px', cursor: 'pointer' }}>Login</button>
+        <button type="submit" style={{ padding: '10px 30px',color:'#000000', cursor: 'pointer',background:'#0ebc70' }}>Login</button>
       </form>
       <br />
       <Link to="/register">Don't have an account? Create one</Link>
